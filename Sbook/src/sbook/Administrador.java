43b8dda91de22ccc1016223022ac13fa637c8344
Lásaro Rocha <lasaro.rocha@gmail.com>
2014-06-03 17:53:27 -0300

    Implementei mais algumas classes.

--- Sbook/src/sbook/Administrador.java
+++ Sbook/src/sbook/Administrador.java
@@ -15,7 +15,7 @@ package sbook;
 public class Administrador extends Usuario {
 
     public Administrador(int codUsuario, String nome, String cpf, String senha) {
-        super(codUsuario, nome, cpf, senha);
+        super(codUsuario ,nome, cpf, senha);
     } 
            
 }
--- Sbook/src/sbook/Sistema.java
+++ Sbook/src/sbook/Sistema.java
@@ -8,15 +8,18 @@ package sbook;
 
 import java.util.LinkedList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Lásaro Rocha
  */
-class Sistema {
+public class Sistema {
     private final List<Administrador> administradores;
     private final List<Usuario> usuarios;
     private final List<ItemAcervo> acervo;
+    private final IAdicionaAdm painelAdicionaAdm;
+    private final ILogin painelLogin;
     
     
     Sistema() {
@@ -24,16 +27,33 @@ class Sistema {
         usuarios = new LinkedList<>();
         acervo = new LinkedList<>();
         
+        painelAdicionaAdm = new IAdicionaAdm();
+        painelLogin = new ILogin();
         iniciaDatabase();
         
     }
     
     void executa() {
-        
+        if(administradores.size() == 0) { //se não tem nenhum administrador, obriga o usuário a criar um.
+            JOptionPane.showMessageDialog(null, "O sistema não tem nenhum administrador. Adicione um.");
+            Administrador adm = painelAdicionaAdm.getAdm();
+            administradores.add(adm);
+        }
+        Usuario u = painelLogin.fazLogin(this);
     }
 
     private void iniciaDatabase() {
         //iniciar a database aqui
+    }
+
+    Usuario loginUsuario(String nome, String senha) {
+        //procura nos administradores
+        for(Administrador adm:administradores) {
+            if(adm.getNome().equals(nome) && adm.getSenha().equals(senha)) {
+                return adm;
+            }
+        }
+        return null;
     }
     
 }
